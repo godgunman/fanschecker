f60ccd8cdde4dd1be95b713f6ea59782ff4bce6d
ggm <godgunman@gmail.com>
2014-04-13 05:19:00 +0800

    rewrite by heroku guide

--- app.js
+++ app.js
@@ -11,7 +11,23 @@ var path = require('path');
 
 var request = require('request');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fanschecker');
+
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.
+var uristring =
+    process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    'mongodb://localhost/fanschecker';
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
 
 var Fans = mongoose.model('Fans', { 
     pageId: String,
